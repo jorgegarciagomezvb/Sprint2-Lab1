c20835d1d4a12097e33d2bd4847fda18ac65d245
Jorge García <jorgegarciagomezvb@gmail.com>
2023-11-23 11:54:16 +0100

    Primer punto terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,24 +4,43 @@ import { urls } from "./constant.js";
   const { posts } = await fetch(urls.post).then((res) => res.json());
   console.log(posts);
 
-  function generatePost(post){
-     const postElemment = document.createElement('article');
-     postElemment.className = "post";
+  const listOfPost = document.querySelector(".posts");
+  listOfPost.innerHTML = "";
+  posts.forEach((post) => listOfPost.appendChild(generatePost(post)));
 
-     const postLink = document.createElement("a");
-     postLink.className = "post-link";
-     postLink.dataset.id = post.id;
+  function generatePost(post) {
+    const postElemment = document.createElement("article");
+    postElemment.className = "post";
 
-      const postHeader = document.createElement('header');
-      postHeader.className = "post-header";
+    const postLink = document.createElement("a");
+    postLink.className = "post-link";
+    postLink.dataset.id = post.id;
 
-    const postHeaderTitle = document.createElement('h3');
+    const postHeader = document.createElement("header");
+    postHeader.className = "post-header";
+
+    const postHeaderTitle = document.createElement("h3");
     postHeaderTitle.textContent = post.title;
     postHeaderTitle.className = "post-title";
 
+    const postDate = document.createElement("p");
+    postDate.textContent = post.date;
+    postDate.className = "post-date";
 
-      const postDate = document.createElement('p');
-      postDate.textContent = formatDataAgo(post.date);
-      postDate.className = "post-date"
+    const postFooter = document.createElement("footer");
+    postFooter.className = "post-footer";
+
+    const postContent = document.createElement("p");
+    postContent.textContent = post.content;
+    postContent.className = "post-content";
+
+    postHeader.appendChild(postHeaderTitle);
+    postFooter.appendChild(postContent);
+    postFooter.appendChild(postDate);
+    postLink.appendChild(postHeader);
+    postLink.appendChild(postFooter);
+    postElemment.appendChild(postLink);
+
+    return postElemment;
   }
 })();
